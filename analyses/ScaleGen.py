cdad4cf2436bffa6c9e2bb1b2e3aa82112ab954c
ThinkNaive <thinknaive@hotmail.com>
2020-02-14 15:33:36 +0800

    参数实验

--- analyses/ScaleGen.py
+++ analyses/ScaleGen.py
@@ -10,9 +10,10 @@ if __name__ == '__main__':
     rows = [500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000]
     col = 10000
     iteration = 10
-    params = [{'id': '1', 'strategy': 'rep', 'p': 10, 'repNum': 2},
-              {'id': '2', 'strategy': 'mds', 'p': 10, 'k': 5},
-              {'id': '3', 'strategy': 'lt', 'p': 10, 'c': 0.03, 'delta': 0.5, 'alpha': 2.0}]
+    params = [{'id': '1', 'strategy': 'rep', 'p': 10, 'repNum': 1},
+              {'id': '2', 'strategy': 'rep', 'p': 10, 'repNum': 2},
+              {'id': '3', 'strategy': 'mds', 'p': 10, 'k': 5},
+              {'id': '4', 'strategy': 'lt', 'p': 10, 'c': 0.03, 'delta': 0.5, 'alpha': 2.0}]
 
     for param in params:
         for row in rows:
@@ -27,11 +28,11 @@ if __name__ == '__main__':
 
             keys, times, comps, stops = run(A, x, iteration, param)
 
-            np.save('statistics/Scale_' + param['strategy'] + '_' + str(row) + '_Exp.npy', time.time() - startTime)
-            np.save('statistics/Scale_' + param['strategy'] + '_' + str(row) + '_Key.npy', keys)
-            np.save('statistics/Scale_' + param['strategy'] + '_' + str(row) + '_Time.npy', times)
-            np.save('statistics/Scale_' + param['strategy'] + '_' + str(row) + '_Comp.npy', comps)
-            np.save('statistics/Scale_' + param['strategy'] + '_' + str(row) + '_Stop.npy', stops)
+            np.save('statistics/Scale_' + param['strategy'] + '_' + param['id'] + '_' + str(row) + '_Exp.npy', time.time() - startTime)
+            np.save('statistics/Scale_' + param['strategy'] + '_' + param['id'] + '_' + str(row) + '_Key.npy', keys)
+            np.save('statistics/Scale_' + param['strategy'] + '_' + param['id'] + '_' + str(row) + '_Time.npy', times)
+            np.save('statistics/Scale_' + param['strategy'] + '_' + param['id'] + '_' + str(row) + '_Comp.npy', comps)
+            np.save('statistics/Scale_' + param['strategy'] + '_' + param['id'] + '_' + str(row) + '_Stop.npy', stops)
 
             print('Average Latency = ' + str(np.mean(stops)))
             print('Run Time = ', str(time.time() - startTime), sep='')
